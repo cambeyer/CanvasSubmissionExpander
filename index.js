2b6300d53ee3feda29679f81d9b8f5676a7175a5
Cameron Beyer <cameron@Camerons-MacBook-Pro.local>
2019-02-19 17:20:06 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const unzip = require('unzip');
+const fs = require('fs');
+const path = require('path');
+
+const canvasGeneratedZipFile = process.argv.splice(process.execArgv.length + 2).join(' ');
+
+const entryParser = (entry, destPath) => {
+    let fileName = entry.path;
+    let dirPath = destPath;
+    if (!destPath) {
+        const fileNameParts = fileName.split('_');
+        const studentName = fileNameParts[0];
+        fileName = fileNameParts.slice(3).join('_');
+        dirPath = path.join(path.dirname(canvasGeneratedZipFile), studentName);
+    }
+    const finalPath = path.join(dirPath, fileName);
+    dirPath = path.dirname(finalPath);
+    fileName = path.basename(finalPath);
+    if (entry.type === 'File') {
+        fs.mkdir(dirPath, { recursive: true }, () => {
+            if (!fileName.endsWith('.zip')) {
+                entry.pipe(fs.createWriteStream(finalPath));
+            } else {
+                entry
+                    .pipe(unzip.Parse())
+                    .on('entry', (entry) => {
+                        entryParser(entry, dirPath);
+                    });
+            }
+        });
+    } else {
+        entry.autodrain();
+    }
+};
+
+fs.createReadStream(canvasGeneratedZipFile)
+    .pipe(unzip.Parse())
+    .on('entry', entryParser);
